b1a3b21c1e23e5730e75d2cd45ac6821467ba0ba
Khushi Sharma <123248338+khushish24@users.noreply.github.com>
2023-06-23 19:30:13 +0530

    Maze Path Problem.
    To reach from certain provided constraints to another with limited number of moves.

---
+++ mazePath.java
@@ -0,0 +1,20 @@
+public class mazePath {
+    static void mazePathProblem(int row , int col, int endRow, int endCol, String result){
+        if(row==endRow && col==endCol){
+            System.out.println(result);
+            return;
+        }
+        if(row>endRow||col>endCol){
+            return;
+        }
+        //move towards right
+        mazePathProblem(row, col+1, endRow, endCol, result+"R");
+        //move towards diagonal
+        mazePathProblem(row+1, col+1, endRow, endCol, result+"I");
+        //move towards down
+        mazePathProblem(row+1, col, endRow, endCol, result+"D");
+    }
+    public static void main(String[] args) {
+        mazePathProblem(0, 0, 2, 2, "");
+    }
+}
